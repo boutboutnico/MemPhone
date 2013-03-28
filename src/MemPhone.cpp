cb1e04ebc322fe0e51a109141aeeec76af17803c
Nicolas BOUTIN <boutwork@gmail.com>
2013-03-28 18:51:46 +0100

    Create repository MemPhone

---
+++ src/MemPhone.cpp
@@ -0,0 +1,205 @@
+//============================================================================
+// Name        : MemPhone.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+// reading a text file
+#include <iostream>
+#include <fstream>
+#include <string>
+
+// vector
+#include <vector>
+#include <map>
+#include <array>
+#include <initializer_list>
+#include <list>
+
+using namespace std;
+
+const vector<vector<char> > v_digit_to_letter { vector<char> { ' ' }, // 0
+        vector<char> { 'i' }, // 1
+        vector<char> { 'a', 'b', 'c' },	// 2
+        vector<char> { 'd', 'e', 'f' },	// 3
+        vector<char> { 'g', 'h', 'i' },	// 4
+        vector<char> { 'j', 'k', 'l' },	// 5
+        vector<char> { 'm', 'n', 'o' },	// 6
+        vector<char> { 'p', 'q', 'r', 's' },	// 7
+        vector<char> { 't', 'u', 'v' },	// 8
+        vector<char> { 'w', 'x', 'y', 'z' },	// 9
+};
+
+void constructListRec(const vector<int>& v_in,
+                      const unsigned int idx_phone_number,
+                      string& s_result,
+                      list<string>& l_result)
+{
+	if(idx_phone_number >= v_in.size())
+	{
+		l_result.push_back(s_result);
+		return;
+	}
+
+	int current_digit = v_in.at(idx_phone_number);
+
+	for(uint32_t i = 0; i < v_digit_to_letter.at(current_digit).size(); i++)
+	{
+		// Append current letter to existing result
+		string s_copy = s_result;
+		s_copy += v_digit_to_letter.at(current_digit).at(i);
+
+		// Call next digit in phone number
+		constructListRec(v_in, idx_phone_number + 1, s_copy, l_result);
+	}
+}
+
+//void constructList(const vector<int>& v_in, list<string>& l_result)
+//{
+//	string s_result;
+//
+//	int cpt = 0;
+//	constructListRec(v_in, cpt, s_result, l_result);
+//
+////	// Loop first digit
+////	uint8_t ui8_current_digit;
+////	for(uint32_t j = 0; j < v_digit_to_letter.at(ui8_current_digit).size(); j++)
+////	{
+////		ui8_current_digit = v_in.at(0);
+////		s_result += v_digit_to_letter.at(ui8_current_digit).at(j);
+////
+////		// Loop second digit
+////		for(uint32_t i = 0; i < v_digit_to_letter.at(ui8_current_digit).size(); i++)
+////		{
+////			ui8_current_digit = v_in.at(1);
+////			string s_temp = s_result + v_digit_to_letter.at(ui8_current_digit).at(i);
+////
+////			l_result.push_back(s_temp);
+////		}
+////		s_result.clear();
+////	}
+//}
+
+int main()
+{
+	// http://www.liste-de-mots.com/mots-nombre-lettre/8/a/
+
+	// File handler
+	string line;
+	ifstream myfile("res/dico_a_8.txt");
+
+	// Vector
+	vector<string> v_dico_8;
+//	vector<string> v_dico_2 { "aa", "ab", "ac", "ad", "ae", "af", "ba", "bb", "bc", "bd", "be", "bf" };
+
+//	vector<int> v_input { 6, 7, 9, 3, 7, 3, 9 };
+//	vector<int> v_input { 6, 4, 4, 6, 6, 6, 6, 3 }; // Mignonne
+//	vector<int> v_input { 3, 2, 1, 3, 8, 7, 2, 8 }; // Mathieu
+	vector<int> v_input { 2, 9, 2, 4, 3, 8, 7, 9 }; //
+//	vector<int> v_input { 3, 1, 7, 3, 3, 6, 8, 8 }; //
+	list<string> l_output;
+
+//	constructList(v_input, l_output);
+
+	string s_result;
+	int cpt = 0;
+	constructListRec(v_input, cpt, s_result, l_output);
+
+	// Display l_output
+	cout << "Display l_output list :" << endl;
+	for(list<string>::iterator it = l_output.begin(); it != l_output.end(); ++it)
+	{
+		cout << *it << endl;
+	}
+	cout << "l_output size : " << l_output.size() << endl;
+
+	// Debug
+//	char X;
+
+	//
+	// Copy file in RAM
+	//
+	if(myfile.is_open())
+	{
+		while(myfile.good())
+		{
+			getline(myfile, line);
+			v_dico_8.push_back(line);
+		}
+		myfile.close();
+	}
+	else cout << "Unable to open file";
+
+	//
+	// Find word
+	//
+	vector<string>::size_type idx = 0;
+//	vector<string>::size_type v_size = v_dico_8.size();
+
+	while(l_output.empty() == false)
+	{
+		string s = l_output.front();
+
+		for(idx = 0; idx < v_dico_8.size(); idx++)
+		{
+			if(s == v_dico_8.at(idx))
+			{
+				cout << "Found : " << s << " " << v_dico_8.at(idx) << endl;
+			}
+		}
+
+		l_output.pop_front();
+	}
+
+	//
+	// Display vector
+	//
+//	for(idx = 0; idx < v_dico_8.size(); idx++)
+//	{
+//		cout << v_dico_8.at(idx) << endl;
+//	}
+//	cout << "v_dico_8 size : " << v_dico_8.size() << endl;
+
+	//
+	// Exit
+	//
+//	while(cin >> X && X != 'q')
+//	{
+//	};
+
+	return 0;
+}
+
+//void constructListRec(const vector<int>& v_in, const unsigned int idx_phone_number, string& s_result, list<string>& l_result)
+//{
+//	static uint32_t cpt = 0;
+////	cout << "ConstructListRec cpt : " << cpt++ << endl;
+//
+////	cout << "ui8_idx_phone_number : " << idx_phone_number << endl;
+//	if(idx_phone_number >= v_in.size())
+//	{
+//		l_result.push_back(s_result);
+////		cout << "Exit" << endl;
+//		return;
+//	}
+//
+//	int current_digit = v_in.at(idx_phone_number);
+////	cout << "current_digit : " << current_digit << endl;
+//
+//	for(uint32_t i = 0; i < v_digit_to_letter.at(current_digit).size(); i++)
+//	{
+////		cout << "i : " << i << endl;
+//
+//		// Append current letter to existing result
+//		string s_copy = s_result;
+//		s_copy += v_digit_to_letter.at(current_digit).at(i);
+////		cout << "s_result : " << s_result << endl;
+//
+//		// Call next digit in phone number
+//		constructListRec(v_in, idx_phone_number + 1, s_copy, l_result);
+//	}
+//
+////	cout << "End of construtListRec" << endl;
+//}
